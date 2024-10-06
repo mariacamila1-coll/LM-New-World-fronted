9bb13285281150800709a8d27be5689f43628cfd
mariacamila1-coll <mariacamila85@gmail.com>
2024-10-06 14:38:56 -0500

    Se añadió estilos al css y vista previa de noticias

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [keyword, setKeyword] = useState('');
+    const [news, setNews] = useState([]);
+
+    const fetchNews = async () => {
+        try {
+            const response = await axios.get(`http://localhost:5000/api/news?keyword=${keyword}`);
+            setNews(response.data.articles);
+        } catch (error) {
+            console.error('Error fetching news:', error);
+        }
+    };
+
+    return (
+        <div className="App">
+            <div className="logo">
+                <img src="src/logoNewsLorox.png" alt="Logo LM News World" className="logo-image" />
+            </div>
+            <h1>LM News World</h1>
+            <div className="search-container">
+                <input 
+                    type="text" 
+                    id="keyword" 
+                    value={keyword}
+                    onChange={(e) => setKeyword(e.target.value)}
+                    placeholder="Buscar noticias..."
+                />
+                <button id="searchBtn" onClick={fetchNews}>Buscar</button>
+            </div>
+
+            <div id="news-container">
+                {news.map((article, index) => (
+                    <div key={index} className="news-item">
+                        <a href={article.url} target="_blank" rel="noopener noreferrer">
+                            <h2>{article.title}</h2>
+                            <p>{article.description}</p>
+                            {article.urlToImage && <img className="news-image" src={article.urlToImage} alt="Imagen de noticia" />}
+                        </a>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
